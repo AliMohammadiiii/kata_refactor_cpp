0d9fc24283013408ce49f08f902e48308674bdb7
alimohammadiiii <1380ali.mohamadi@gmail.com>
2023-07-07 13:26:42 +0330

    Improve Code Organization

--- cpp/src/ReceiptPrinter.h
+++ cpp/src/ReceiptPrinter.h
@@ -9,30 +9,21 @@
 
 class ReceiptPrinter
 {
-
 public:
     ReceiptPrinter(int columns = 40);
-
     std::string printReceipt(const Receipt &receipt);
 
+private:
     std::string presentReceiptItem(const ReceiptItem &item) const;
-
     std::string presentDiscount(const Discount &discount) const;
-
     std::string presentTotal(const Receipt &receipt) const;
 
     std::string formatLineWithWhitespace(const std::string &name, const std::string &value) const;
+    std::string presentPrice(double price) const;
+    std::string presentQuantity(const ReceiptItem &item);
+    std::string getFormattedNumberAsString(double number, int precision);
 
-    static std::string presentQuantity(const ReceiptItem &item);
-
-private:
-
-    static std::string getFormattedNumberAsString(double number, int precision = 2);
-
-    const int columns;
-
-
+    int columns;
 };
-
 
 #endif //CPP_RECEIPTPRINTER_H
